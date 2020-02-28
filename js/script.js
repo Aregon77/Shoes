022b94ad51e19cb88178ee50237fd0d53d4b1091
Valerik <Aregon7777@yandex.ru>
2020-02-28 14:19:13 +0500

    Анимация

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
     $('.reviews_slider').slick();
   });
+
+$('.burger-menu').on('click', function(e) {
+  e.preventDefault;
+  $(this).toggleClass('burger-menu_active');
+  $('.nav__link').toggleClass('nav__link_active');
+});  
