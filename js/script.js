de52e382c7fe54e0503781241c4a61d30a70a28d
Valerik <Aregon7777@yandex.ru>
2020-03-03 17:20:38 +0500

    футер

--- js/script.js
+++ js/script.js
@@ -6,4 +6,12 @@ $('.burger-menu').on('click', function(e) {
   e.preventDefault;
   $(this).toggleClass('burger-menu_active');
   $('.nav__link').toggleClass('nav__link_active');
-});  
+});
+
+$(document).ready(function(){
+    $('.footer__title').click(function(event){
+      $(this).toggleClass('active').next().slideToggle(1).css({
+        display:'flex'
+      });
+    }); 
+});
